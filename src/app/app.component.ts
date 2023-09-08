22bc37cacaf0eedde488b249f4df3414a767a91b
Biju Kumar <biju.official@gmail.com>
2023-09-08 21:28:46 +0530

    Added base project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  weather = 'Todays temperature is 32C';
+}
